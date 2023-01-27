c8f9d36722645990e32054611f9df796ae84a46f
Peter Chu <chucheng801203@gmail.com>
2023-01-27 14:44:48 +0800

    修改連線檢查方式

--- src/WsClient.php
+++ src/WsClient.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Peterchu;
 
 use Exception;
@@ -37,13 +39,17 @@ class WsClient
      */
     private int $streamTimeout = 5;
     /**
+     * 與 server 建立連線時 timeout, 預設10秒
+     */
+    private int $connectTimeout = 10;
+    /**
      * 確認與 server 連線狀態間隔秒數
      */
-    private int $keepAliveInterval = 180;
+    private int $keepAliveInterval = 10;
     /**
      * 過幾秒沒收到 Pong 就中斷連線
      */
-    private int $maxReceivePongSeconds = 600;
+    private int $maxReceivePongSeconds = 30;
     /**
      * 最後一次 ping 的時間
      */
@@ -52,10 +58,6 @@ class WsClient
      * 最早 ping 的時間, 收到 pong 時重置
      */
     private int $firstPingTime = 0;
-    /**
-     * 與 server 建立連線時 timeout, 預設10秒
-     */
-    private int $connectTimeout = 10;
     /**
      * 有效的 pong payload data
      */
@@ -208,13 +210,13 @@ class WsClient
             list($frameType, $message) = $this->read();
 
             if ($frameType === self::CLOSE_MESSAGE) {
-                if (is_resource($this->stream)) {
-                    if (!feof($this->stream)) {
-                        $this->write(self::CLOSE_MESSAGE, pack('n*', $message['code'] === null ? self::CLOSE_NORMAL_CLOSURE : $message['code']));
-                    }
-
-                    fclose($this->stream);
+                try {
+                    $this->write(self::CLOSE_MESSAGE, pack('n', $message['code'] === null ? self::CLOSE_NORMAL_CLOSURE : $message['code']));
+                } catch (Throwable $th) {
+                    break;
                 }
+
+                $this->closeStream();
 
                 echo 'WsClient: info: receive close frame: ' . json_encode($message) . "\n";
 
@@ -314,122 +316,54 @@ class WsClient
     /**
      * 處理 websocket frame
      *
-     * @param bool $withoutPingPong 不檢查 ping pong
+     * @param bool $checkIsAlive 是否檢查 ping pong
      * @param int|null $timeout 秒數
      * @return array
      * @throws Exception
      */
-    public function read(bool $withoutPingPong = false, int $timeout = null): array
+    public function read(bool $checkIsAlive = true, int $timeout = null): array
     {
         $finalFragment = false;
         $frameType = null;
         $message = '';
 
-        if ($timeout !== null) {
-            $startTime = time();
-        }
-
         while ($finalFragment === false) {
-            $bytes = $this->readBytes(2, $withoutPingPong, $timeout === null ? null : $timeout - (time() - $startTime));
+            $frame = $this->readNextFrame($checkIsAlive, $timeout);
 
-            $byte1 = unpack('C*', $bytes[0]);
-            $byte2 = unpack('C*', $bytes[1]);
-
-            // 解析 frame header(RFC 6455, section 5.2)
-            $opcode = $byte1[1] & 0x0f;
-            $fin = ($byte1[1] & 0x80) != 0;
-            $rsv1 = ($byte1[1] & 0x40) != 0;
-            $rsv2 = ($byte1[1] & 0x20) != 0;
-            $rsv3 = ($byte1[1] & 0x10) != 0;
-            $mask = ($byte2[1] & 0x80) != 0;
-            $payloadLength = $byte2[1] & 0x7f;
-
-            $errors = [];
-
-            if ($rsv1) {
-                $errors[] = 'RSV1 must be 0';
-            }
-
-            if ($rsv2) {
-                $errors[] = 'RSV2 must be 0';
-            }
-
-            if ($rsv3) {
-                $errors[] = 'RSV3 must be 0';
-            }
-
-            switch ($opcode) {
-                case self::CLOSE_MESSAGE:
-                case self::PING_MESSAGE:
-                case self::PONG_MESSAGE:
-                    // RFC 6455, section 5.5
-                    if ($payloadLength > 125) {
-                        $errors[] = 'control frames must have a payload length of 125 bytes or less';
-                    }
-
-                    if (!$fin) {
-                        $errors[] = 'control frames must not be fragmented';
-                    }
-
-                    break;
+            switch ($frame['opcode']) {
                 case self::TEXT_MESSAGE:
                 case self::BINARY_MESSAGE:
                     if ($frameType !== null) {
-                        $errors[] = 'receive text frame or binary frame in the middle of a fragmented message';
+                        throw new Exception('WsClient: receive text frame or binary frame in the middle of a fragmented message');
                     }
 
-                    $finalFragment = $fin;
-                    $frameType = $opcode;
+                    $finalFragment = $frame['fin'];
+                    $frameType = $frame['opcode'];
 
                     break;
                 case self::CONTINUATION_FRAME:
                     if ($frameType === null) {
-                        $errors[] = 'receive continuation frame before text frame or binary frame';
+                        throw new Exception('WsClient: receive continuation frame before text frame or binary frame');
                     }
 
-                    $finalFragment = $fin;
-
-                    break;
-                default:
-                    $errors[] = 'unknown opcode is received';
+                    $finalFragment = $frame['fin'];
 
                     break;
             }
 
-            if (count($errors) > 0) {
-                $errMsg = implode(', ', $errors);
-                throw new Exception("WsClient: $errMsg");
+            if ($frame['opcode'] === self::CONTINUATION_FRAME || $frame['opcode'] === self::TEXT_MESSAGE || $frame['opcode'] === self::BINARY_MESSAGE) {
+                $message .= $frame['payload'];
             }
 
-            if ($payloadLength === 126) {
-                $bytes = $this->readBytes(2, $withoutPingPong, $timeout === null ? null : $timeout - (time() - $startTime));
-
-                $data = unpack('n*', $bytes);
-                $payloadLength = $data[1];
-            } else if ($payloadLength === 127) {
-                $bytes = $this->readBytes(8, $withoutPingPong, $timeout === null ? null : $timeout - (time() - $startTime));
-
-                $data = unpack('J*', $bytes);
-                $payloadLength = $data[1];
-            }
-
-            $payload = $this->readBytes($payloadLength, $withoutPingPong, $timeout === null ? null : $timeout - (time() - $startTime));
-
-            if ($opcode === self::CONTINUATION_FRAME || $opcode === self::TEXT_MESSAGE || $opcode === self::BINARY_MESSAGE) {
-                $message .= $payload;
-            }
-
-            switch ($opcode) {
+            switch ($frame['opcode']) {
                 case self::CLOSE_MESSAGE:
                     // RFC 6455, section 5.5.1
-                    $code = strlen($payload) >= 2 ? unpack('n*', substr($payload, 0, 2)) : [1 => null];
-                    $reason = strlen($payload) > 2 ? utf8_decode(substr($payload, 2)) : null;
+                    $code = strlen($frame['payload']) >= 2 ? unpack('n', substr($frame['payload'], 0, 2)) : [1 => null];
+                    $reason = strlen($frame['payload']) > 2 ? substr($frame['payload'], 2) : null;
 
                     return [self::CLOSE_MESSAGE, ['code' => $code[1], 'reason' => $reason]];
                 case self::PING_MESSAGE:
-                    if (!$withoutPingPong) {
-                        $this->write(self::PONG_MESSAGE, $payload);
-                    }
+                    $this->write(self::PONG_MESSAGE, $frame['payload']);
 
                     if ($frameType === null) {
                         return [self::NO_FRAME, ''];
@@ -437,16 +371,14 @@ class WsClient
 
                     break;
                 case self::PONG_MESSAGE:
-                    if (!$withoutPingPong) {
-                        $now = time();
-                        $maxReceivePongSeconds = $this->maxReceivePongSeconds;
-                        $this->validPongPayloadData = array_values(array_filter($this->validPongPayloadData, function($t) use($maxReceivePongSeconds, $now) {
-                            return $now - $t <= $maxReceivePongSeconds;
-                        }));
+                    $now = time();
+                    $maxReceivePongSeconds = $this->maxReceivePongSeconds;
+                    $this->validPongPayloadData = array_values(array_filter($this->validPongPayloadData, function($t) use($maxReceivePongSeconds, $now) {
+                        return $now - $t <= $maxReceivePongSeconds;
+                    }));
 
-                        if (in_array(intval($payload), $this->validPongPayloadData)) {
-                            $this->firstPingTime = 0;
-                        }
+                    if (in_array(intval($frame['payload']), $this->validPongPayloadData)) {
+                        $this->firstPingTime = 0;
                     }
 
                     if ($frameType === null) {
@@ -461,15 +393,116 @@ class WsClient
     }
 
     /**
+     * 讀取下一個 frame
+     *
+     * @param bool $checkIsAlive 是否檢查 ping pong
+     * @param int|null $timeout 秒數
+     * @return array
+     * @throws Exception
+     */
+    public function readNextFrame(bool $checkIsAlive = true, int $timeout = null): array
+    {
+        if ($timeout !== null) {
+            $startTime = time();
+        }
+
+        $bytes = $this->readBytes(2, $checkIsAlive, $timeout === null ? null : $timeout - (time() - $startTime));
+
+        $byte1 = unpack('C', $bytes[0]);
+        $byte2 = unpack('C', $bytes[1]);
+
+        // 解析 frame header(RFC 6455, section 5.2)
+        $opcode = $byte1[1] & 0x0f;
+        $fin = ($byte1[1] & 0x80) != 0;
+        $rsv1 = ($byte1[1] & 0x40) != 0;
+        $rsv2 = ($byte1[1] & 0x20) != 0;
+        $rsv3 = ($byte1[1] & 0x10) != 0;
+        $mask = ($byte2[1] & 0x80) != 0;
+        $payloadLength = $byte2[1] & 0x7f;
+
+        $errors = [];
+
+        if ($rsv1) {
+            $errors[] = 'RSV1 must be 0';
+        }
+
+        if ($rsv2) {
+            $errors[] = 'RSV2 must be 0';
+        }
+
+        if ($rsv3) {
+            $errors[] = 'RSV3 must be 0';
+        }
+
+        switch ($opcode) {
+            case self::CLOSE_MESSAGE:
+            case self::PING_MESSAGE:
+            case self::PONG_MESSAGE:
+                // RFC 6455, section 5.5
+                if ($payloadLength > 125) {
+                    $errors[] = 'control frames must have a payload length of 125 bytes or less';
+                }
+
+                if (!$fin) {
+                    $errors[] = 'control frames must not be fragmented';
+                }
+
+                break;
+            case self::TEXT_MESSAGE:
+            case self::BINARY_MESSAGE:
+            case self::CONTINUATION_FRAME:
+
+                break;
+            default:
+                $errors[] = 'unknown opcode is received';
+
+                break;
+        }
+
+        if (count($errors) > 0) {
+            $errMsg = implode(', ', $errors);
+            throw new Exception("WsClient: $errMsg");
+        }
+
+        if ($payloadLength === 126) {
+            $bytes = $this->readBytes(2, $checkIsAlive, $timeout === null ? null : $timeout - (time() - $startTime));
+
+            $data = unpack('n', $bytes);
+            $payloadLength = $data[1];
+        } else if ($payloadLength === 127) {
+            $bytes = $this->readBytes(8, $checkIsAlive, $timeout === null ? null : $timeout - (time() - $startTime));
+
+            $data = unpack('J', $bytes);
+            $payloadLength = $data[1];
+        }
+
+        $payload = $this->readBytes($payloadLength, $checkIsAlive, $timeout === null ? null : $timeout - (time() - $startTime));
+
+        return [
+            'fin' => $fin,
+            'rsv1' => $rsv1,
+            'rsv2' => $rsv2,
+            'rsv3' => $rsv3,
+            'opcode' => $opcode,
+            'mask' => $mask,
+            'payloadLength' => $payloadLength,
+            'payload' => $payload,
+        ];
+    }
+
+    /**
      * websocket send
      *
      * @param int $frameType
      * @param string $message
-     * @return void
      * @throws Exception
      */
     public function write(int $frameType, string $message = ''): void
     {
+        if (!is_resource($this->stream)) {
+            throw new Exception('WsClient: WsClient::stream is not a resource');
+        }
+
         switch ($frameType) {
             case self::TEXT_MESSAGE:
                 $opcode = 0x1;
@@ -525,61 +558,37 @@ class WsClient
 
         $frame .= $payload;
 
-        fwrite($this->stream, $frame, strlen($frame));
+        $length = strlen($frame);
+
+        $result = fwrite($this->stream, $frame, $length);
+
+        if ($result === false || $result !== $length) {
+            throw new Exception('WsClient: sending data failed');
+        }
     }
 
     /**
-     * 檢查是否有在時限內收到 pong frame
+     * 定時發送 ping frame 和檢查是否有在時限內收到 pong frame
      *
      * @throws Exception
      */
     public function checkIsAlive(): void
     {
-        if ($this->firstPingTime !== 0) {
-            $now = time();
-
-            if ($now - $this->firstPingTime > $this->maxReceivePongSeconds) {
-                throw new Exception('WsClient: no pong received from server');
-            }
-        }
-    }
-
-    /**
-     * 檢查連線 resource
-     *
-     * @throws Exception
-     */
-    public function checkStream(): void
-    {
-        if (!is_resource($this->stream)) {
-            throw new Exception('WsClient: connection is not established');
-        }
-
-        if (feof($this->stream)) {
-            throw new Exception('WsClient: connection exception and disconnect from server');
-        }
-    }
-
-    /**
-     * 檢查是否需要發送 ping frame
-     *
-     * @throws Exception
-     */
-    public function sendPingFrame(): void
-    {
         $now = time();
 
-        if ($this->lastPingTime === 0) {
-            $this->lastPingTime = $now;
-        }
-
         if ($now - $this->lastPingTime > $this->keepAliveInterval) {
-            $this->write(self::PING_MESSAGE, $now);
+            $this->write(self::PING_MESSAGE, (string)$now);
             $this->lastPingTime = $now;
             $this->validPongPayloadData[] = $now;
 
             if ($this->firstPingTime === 0) {
                 $this->firstPingTime = $now;
+            }
+        }
+
+        if ($this->firstPingTime !== 0) {
+            if ($now - $this->firstPingTime > $this->maxReceivePongSeconds) {
+                throw new Exception('WsClient: no pong received from server');
             }
         }
     }
@@ -591,6 +600,8 @@ class WsClient
      */
     public function connect(): void
     {
+        $this->disconnect();
+
         // 進行 handshake
         $this->handshake();
 
@@ -609,27 +620,25 @@ class WsClient
     public function disconnect(int $statusCode = self::CLOSE_NORMAL_CLOSURE, string $reason = ''): void
     {
         if (is_resource($this->stream)) {
-            if (!feof($this->stream)) {
-                $this->write(self::CLOSE_MESSAGE, pack('n*', $statusCode) . $reason);
+            $this->write(self::CLOSE_MESSAGE, pack('n', $statusCode) . $reason);
 
-                $startTime = time();
+            $startTime = time();
 
-                while (true) {
-                    $remainTime = 10 - (time() - $startTime);
+            while (true) {
+                $remainTime = 10 - (time() - $startTime);
 
-                    if ($remainTime <= 0) {
-                        break;
-                    }
+                if ($remainTime <= 0) {
+                    break;
+                }
 
-                    try {
-                        list($frameType) = $this->read(true, $remainTime);
-                    } catch (Throwable $th) {
-                        break;
-                    }
+                try {
+                    $frame = $this->readNextFrame(false, $remainTime);
+                } catch (Throwable $th) {
+                    break;
+                }
 
-                    if ($frameType === self::CLOSE_MESSAGE) {
-                        break;
-                    }
+                if ($frame['opcode'] === self::CLOSE_MESSAGE) {
+                    break;
                 }
             }
 
@@ -655,26 +664,31 @@ class WsClient
      * 讀取 Bytes
      *
      * @param int $bytes
-     * @param bool $withoutPingPong 不檢查 ping pong
+     * @param bool $checkIsAlive 是否檢查 ping pong
      * @param int|null $timeout 秒數
      * @return string
      * @throws Exception
      */
-    public function readBytes(int $bytes, bool $withoutPingPong = false, int $timeout = null): string
+    public function readBytes(int $bytes, bool $checkIsAlive = true, int $timeout = null): string
     {
         $readLength = 1024;
         $data = '';
 
         if ($timeout !== null) {
+            if ($timeout <= 0) {
+                throw new Exception('WsClient: read timeout');
+            }
+
             $startTime = time();
         }
 
         while (($read = strlen($data)) < $bytes) {
-            $this->checkStream();
+            if (!is_resource($this->stream)) {
+                throw new Exception('WsClient: WsClient::stream is not a resource');
+            }
 
-            if (!$withoutPingPong) {
+            if ($checkIsAlive) {
                 $this->checkIsAlive();
-                $this->sendPingFrame();
             }
 
             if ($bytes - $read < $readLength) {
